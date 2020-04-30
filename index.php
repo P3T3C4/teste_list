0a5e8883a458e7b9ccd70be2267a19110a18d22a
Joel Santana <p3t3ca5206@gmail.com>
2020-04-30 02:43:42 -0300

    pao com vinagrete

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css"href="style.css" />
+    </head>
+    <body>
+    <div id="app">
+        <ul></ul>
+
+        <input type="text" placeholder="Digite oq vc tem q fazer" >
+        <button>Adicionar</button>
+    </div>
+    
+        <script src="script.js"></script>
+
+    </body>
+</html>
